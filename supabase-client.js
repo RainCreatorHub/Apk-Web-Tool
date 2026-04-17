533f48edd6856d74499776d108febf499f9a9f9f
RainCreatorHub <rain.hub.creator@gmail.com>
2026-04-17 09:02:11 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'url';
+const supabaseUrl = 'https://hlxctwnlwvoyyntptoxk.supabase.co';
 const supabaseKey = 'anon pub';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
